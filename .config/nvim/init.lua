adcd658bd7be47e55d298b8b971deec1fa893b1f
Matthias Brukner <matthias.brukner@surecam.com>
2025-02-18 22:35:09 +0100

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -571,11 +571,11 @@ cmp.setup {
 }
 
 require('rose-pine').setup({
-        --- @usage 'auto'|'main'|'moon'|'dawn'
-        variant = 'auto',
-        --- @usage 'main'|'moon'|'dawn'
-        dark_variant = 'main',
-        disable_italics = true,
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'auto',
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = 'main',
+  disable_italics = true,
 })
 
 --vim.cmd('colorscheme rose-pine')
